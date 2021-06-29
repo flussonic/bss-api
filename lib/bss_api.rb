86d719d03cca6f696217bc2c88932766cf581a9a
Kirill Usanov <kirill.usanov.dev@gmail.com>
2021-06-29 10:57:55 +0300

    Require files

--- lib/bss_api.rb
+++ lib/bss_api.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative 'bss_api/version'
+require_relative 'bss_api/data_collector'
+require_relative 'bss_api/hostable_data_collector'
+require_relative 'bss_api/decorators/base'
+require 'app/controllers/base_controller'
 
 module BssApi
   class NotAllowedAttributesError < StandardError; end
