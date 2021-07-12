a9b7337dc88c3bfc84ca99f465c52eeacba58ab7
Kirill Usanov <kirill.usanov.dev@gmail.com>
2021-07-12 18:54:29 +0300

    Fixed params validation specs

--- lib/bss_api/spec/support/shared_examples/index_params_validation.rb
+++ lib/bss_api/spec/support/shared_examples/index_params_validation.rb
@@ -7,36 +7,39 @@ RSpec.shared_examples 'BSS: index params validation' do |api_key, methods:, forb
     context 'JSON' do
       it 'raises exception with invalid select params' do
         public_send(method, :index, params: { select: "#{forbidden_fields_string},id" }, format: :json)
-        expect(json_response).to eq('error' => "Attributes #{forbidden_fields} not allowed for selecting.")
+        expect(json_response).to eq('error' =>
+                                      "Attributes [#{forbidden_fields.sort.join(', ')}] not allowed for selecting.")
       end
 
       it 'raises exception with invalid sort params' do
         public_send(method, :index, params: { sort: "#{forbidden_fields_string},id" }, format: :json)
-        expect(json_response).to eq('error' => "Attributes #{forbidden_fields} not allowed for sorting.")
+        expect(json_response).to eq('error' =>
+                                      "Attributes [#{forbidden_fields.sort.join(', ')}] not allowed for sorting.")
       end
 
       it 'raises exception with invalid filter params' do
         params = Hash[forbidden_fields.map { |f| [f.to_sym, rand(10)] }]
         public_send(method, :index, params: params.merge(id: rand(10)), format: :json)
-        expect(json_response).to eq('error' => "Attributes #{forbidden_fields} not allowed for filtering.")
+        expect(json_response).to eq('error' =>
+                                     "Attributes [#{forbidden_fields.sort.join(', ')}] not allowed for filtering.")
       end
     end
 
     context 'CSV' do
       it 'raises exception with invalid select params' do
         public_send(method, :index, params: { select: "#{forbidden_fields_string},id" }, format: :csv)
-        expect(csv_response).to eq([["Attributes #{forbidden_fields} not allowed for selecting."]])
+        expect(csv_response).to eq([["Attributes [#{forbidden_fields.sort.join(', ')}] not allowed for selecting."]])
       end
 
       it 'raises exception with invalid sort params' do
         public_send(method, :index, params: { sort: "#{forbidden_fields_string},id" }, format: :csv)
-        expect(csv_response).to eq([["Attributes #{forbidden_fields} not allowed for sorting."]])
+        expect(csv_response).to eq([["Attributes [#{forbidden_fields.sort.join(', ')}] not allowed for sorting."]])
       end
 
       it 'raises exception with invalid filter params' do
         params = Hash[forbidden_fields.map { |f| [f.to_sym, rand(10)] }]
         public_send(method, :index, params: params.merge(id: rand(10)), format: :csv)
-        expect(csv_response).to eq([["Attributes #{forbidden_fields} not allowed for filtering."]])
+        expect(csv_response).to eq([["Attributes [#{forbidden_fields.sort.join(', ')}] not allowed for filtering."]])
       end
     end
   end
